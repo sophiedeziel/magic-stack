90fef2ad57d41b75ff66200c06247312005d61fc
Sophie Déziel <courrier@sophiedeziel.com>
2023-08-24 22:42:36 -0400

    Add bin/check

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,8 +1,8 @@
-import ReactOnRails from 'react-on-rails';
+import ReactOnRails from 'react-on-rails'
 
-import App from '../ui/App';
+import App from '../ui/App'
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  App,
-});
+  App
+})
--- app/javascript/ui/AppServer.js
+++ app/javascript/ui/AppServer.js
@@ -1,5 +1,5 @@
-import App from './App';
+import App from './App'
 // This could be specialized for server rendering
 // For example, if using React-Router, we'd have the SSR setup here.
 
-export default App;
+export default App
